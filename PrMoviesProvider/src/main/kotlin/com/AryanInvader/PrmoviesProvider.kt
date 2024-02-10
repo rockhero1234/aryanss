ccf9f299c86f7c42dc28cddcf4b00f356bccf89a
rockhero1234 <149141736+rockhero1234@users.noreply.github.com>
2024-02-10 17:54:18 +0530

    Update PrmoviesProvider.kt

--- PrMoviesProvider/src/main/kotlin/com/AryanInvader/PrmoviesProvider.kt
+++ PrMoviesProvider/src/main/kotlin/com/AryanInvader/PrmoviesProvider.kt
@@ -11,7 +11,7 @@ import org.jsoup.nodes.Element
 class PRMoviesProvider : MainAPI() {
 
     override var mainUrl = "https://prmovies.wine"
-    override var name = "pr"
+    override var name = "prm"
     override val hasMainPage = true
     override var lang = "hi"
     override val hasQuickSearch = true
